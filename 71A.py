2daa6d746969198271ea303cb861c488e35ccb4f
ninja <astore.a.fabio@gmail.com>
2025-05-10 00:22:33 -0300

    71A.py

---
+++ 71A.py
@@ -0,0 +1,7 @@
+n = int(input())
+for i in range(n):
+    word = str(input())
+    word_size = len(word)
+    if word_size > 10:
+        word = word[0] + str(word_size - 2) + word[-1]
+    print(word)
